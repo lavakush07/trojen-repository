dd0a9d345d009611f0f01090b1fe2b422ff24e1b
Lavakush biyani <70131581+lavakush07@users.noreply.github.com>
2025-11-26 09:00:37 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+console.log("Demo Web App Loaded");
+
+// Importing packages (not executed)
+import 'is';
+import 'canvas-constructor';
+import 'eventsource';
+import 'ua-parser-js';
+import 'eslint-config-prettier';
+
+console.log("Packages imported for demo purposes only.");
+
